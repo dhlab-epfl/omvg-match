212e3f1e43415c9f11945e4861f5dc29397659c2
nils-hamel <nils.hamel@bluewin.ch>
2019-04-26 11:36:10 +0200

    Adding source header license

--- src/omvg-match.cpp
+++ src/omvg-match.cpp
@@ -1,3 +1,22 @@
+/*
+ *  omvg-match
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Copyright (c) 2019 DHLAB, EPFL
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
     # include <iostream>
 
@@ -32,7 +51,7 @@
             /* extract match index */
             const auto& mvg_i = mvg_cur_match.first.first;
             const auto& mvg_j = mvg_cur_match.first.second;
-        
+
             /* display match index */
             std::cout << mvg_i << " " << mvg_j << std::endl;
 
